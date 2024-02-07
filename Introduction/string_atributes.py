8050c5e65a6ae7b27f17cac308b3932cfebd4f1b
osaimahesh21 <121642261+osaimahesh21@users.noreply.github.com>
2024-02-07 22:39:41 +0000

    splitting , concating , and joining ,strings in python

---
+++ Introduction/string_atributes.py
@@ -0,0 +1,22 @@
+
+"""sentence = "Hello, world , how are you  "
+words = sentence.split() # Defaults to splitting on Whitespaces
+print("words=" , words)
+date = "10-05-1998"
+splitwordsofdate = date.split('-')
+print("splitwordsofdate =" ,splitwordsofdate)
+myname = "sai mahesh obillaneni"
+splitofmyname = myname.split('-')
+print("splitofmyname =" ,splitofmyname)
+paragraph = "   Paragraphs are the building blocks of papers   """
+#splitofparagraph = paragraph.split()
+#print("splitofparagraph" ,splitofparagraph)
+
+
+
+#splitting , Concating, and Joining ,stringd in python
+sentance = "    It is used to spl-it a string into a list of substrings based on a specified delimiter.   "
+splitsentance = sentance.split()
+print("splitsentance = " , splitsentance)
+length = len(splitsentance)
+print("lenth of the string" , length)
